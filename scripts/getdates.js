f21f2e3b37c51adcabce73dec559495b264a043a
hermiliomota <hmota1@byupathway.edu>
2026-01-10 23:49:31 +0000

    audit

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,7 +1,3 @@
-// getdates.js
-
-// Preenche o ano atual
-document.getElementById("currentyear").textContent = new Date().getFullYear();
-
-// Preenche a data de última modificação
-document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
+const currentYear = new Date().getFullYear();
+document.getElementById("currentyear").textContent = currentYear;
+document.getElementById("lastModified").textContent = document.lastModified;
